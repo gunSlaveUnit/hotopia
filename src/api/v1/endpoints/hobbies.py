65bc5cba5af3b7066603d0aacbe2af9ce191be7b
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-11-11 14:27:26 +0300

    Added response model for items route in hobby router. Dummy data modified to be in consistency with validation schema

--- src/api/v1/endpoints/hobbies.py
+++ src/api/v1/endpoints/hobbies.py
@@ -1,19 +1,26 @@
+from typing import List
+
 from fastapi import APIRouter
 
 from src.settings import HOBBIES_ROUTER_PREFIX
+from src.api.v1.schemas.hobbies import HobbyBaseSchema
 
 router = APIRouter(prefix=HOBBIES_ROUTER_PREFIX)
 
 
-@router.get('/')
+@router.get('/', response_model=List[HobbyBaseSchema])
 async def items():
     return [
         {
             "id": 0,
+            "created_at": 1699701695,
+            "updated_at": None,
             "name": "Airsoft",
         },
         {
             "id": 1,
+            "created_at": 1699701695,
+            "updated_at": None,
             "name": "Card tricks",
         },
     ]
