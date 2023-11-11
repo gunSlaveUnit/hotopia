c27641033306797abc1ea932ba2ac561d323216f
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-11-11 12:36:17 +0300

    V1 api router

---
+++ api/v1/api.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter
+
+from settings import API_VERSION_1_PREFIX
+
+from api.v1.endpoints.hobbies import router as hobbies_router
+
+router = APIRouter(prefix=API_VERSION_1_PREFIX)
+
+router.include_router(hobbies_router)
