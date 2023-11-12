6aaf1f031a9a7a14dcac3526e0e0ad7767172f95
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-11-12 23:49:10 +0300

    Function to get an async db session

--- server/src/core/utils/db.py
+++ server/src/core/utils/db.py
@@ -1,5 +1,19 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession, AsyncEngine
 
 engine: AsyncEngine = create_async_engine(
     "postgresql+asyncpg://postgres:postgres@localhost:5432/hotopia"
 )
+
+
+async def get_db() -> AsyncSession:
+    session_maker = async_sessionmaker(
+        bind=engine,
+        expire_on_commit=False,
+        autoflush=False
+    )
+
+    async with session_maker() as session:
+        try:
+            yield session
+        finally:
+            await session.close()
