0011c65bc94f6a4385ddc7229f383d6662532d88
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-11-12 23:51:34 +0300

    Method to create an Entity using an async session

--- server/src/core/models/entity.py
+++ server/src/core/models/entity.py
@@ -1,6 +1,7 @@
 import datetime
 
 from sqlalchemy import func
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Mapped, mapped_column
 
 from core.utils.db import Base
@@ -12,3 +13,12 @@ class Entity(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     created_at: Mapped[datetime.datetime] = mapped_column(server_default=func.now())
     updated_at: Mapped[datetime.datetime] = mapped_column(server_default=None, onupdate=func.now(), nullable=True)
+
+    @classmethod
+    async def create(cls, session: AsyncSession, data):
+        item = cls(**vars(data))
+
+        async with session.begin():
+            session.add(item)
+            await session.flush()
+            return item
