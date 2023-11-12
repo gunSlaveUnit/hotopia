e395c54e424b7a5ff844efcb577ec3d8580330d8
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-11-12 23:50:40 +0300

    Create db on startup

--- server/src/app.py
+++ server/src/app.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
 
+from core.utils.db import create as create_db
 from server.src.api.v1.api import router as api_v1_router
 
 app = FastAPI(debug=True)
 
 app.include_router(api_v1_router)
+
+
+@app.on_event("startup")
+async def startup_event() -> None:
+    await create_db()
