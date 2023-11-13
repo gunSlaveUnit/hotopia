bfe60a4caea5ab54db09b11a44def0c7dbe3026d
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-11-13 17:34:10 +0300

    Base module schema

---
+++ server/src/api/v1/schemas/modules.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+
+from server.src.api.v1.schemas.entity import EntityDBSchema
+
+
+class ModuleBaseSchema(BaseModel):
+    name: str
+    description: str
+    done: bool
+    hobby: int
+    previous_module: int | None
