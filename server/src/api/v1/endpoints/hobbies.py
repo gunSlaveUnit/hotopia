7f2481a3300bf646103716567faff92e7bde7136
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-11-13 23:10:43 +0300

    Real db hobbies instead of dummy data

--- server/src/api/v1/endpoints/hobbies.py
+++ server/src/api/v1/endpoints/hobbies.py
@@ -3,30 +3,17 @@ from typing import List
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from core.models.hobbies import Hobby
 from core.utils.db import get_db
-from server.src.api.v1.schemas.hobbies import HobbyDBSchema, HobbyCreateSchema
+from core.models.hobbies import Hobby
 from server.src.settings import HOBBIES_ROUTER_PREFIX
+from server.src.api.v1.schemas.hobbies import HobbyDBSchema, HobbyCreateSchema
 
 router = APIRouter(prefix=HOBBIES_ROUTER_PREFIX)
 
 
 @router.get('/', response_model=List[HobbyDBSchema])
-async def items():
-    return [
-        {
-            "id": 0,
-            "created_at": 1699701695,
-            "updated_at": None,
-            "name": "Airsoft",
-        },
-        {
-            "id": 1,
-            "created_at": 1699701695,
-            "updated_at": None,
-            "name": "Card tricks",
-        },
-    ]
+async def items(db: AsyncSession = Depends(get_db)) -> List[Hobby]:
+    return [_ async for _ in Hobby.every(db)]
 
 
 @router.get('/{item_id}', response_model=HobbyDBSchema)
