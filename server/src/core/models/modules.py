3ae040acef6bc9091ac713a052549938b5466430
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-11-13 17:32:44 +0300

    Module model

---
+++ server/src/core/models/modules.py
@@ -0,0 +1,15 @@
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+
+from core.models.entity import Entity
+
+
+class Module(Entity):
+    __tablename__ = "modules"
+
+    name: Mapped[str]
+    description: Mapped[str]
+    done: Mapped[bool]
+
+    hobby: Mapped[int] = mapped_column("hobby_id", ForeignKey("hobbies.id"))
+    previous_module: Mapped[int] = mapped_column("module_id", ForeignKey("modules.id"), nullable=True)
