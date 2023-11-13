ebfe9705ca14c0a7a35576a120ce15e205955977
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-11-13 17:35:40 +0300

    Bound modules router

--- server/src/api/v1/api.py
+++ server/src/api/v1/api.py
@@ -2,7 +2,9 @@ from fastapi import APIRouter
 
 from settings import API_VERSION_1_PREFIX
 from server.src.api.v1.endpoints.hobbies import router as hobbies_router
+from server.src.api.v1.endpoints.modules import router as modules_router
 
 router = APIRouter(prefix=API_VERSION_1_PREFIX)
 
 router.include_router(hobbies_router)
+router.include_router(modules_router)
