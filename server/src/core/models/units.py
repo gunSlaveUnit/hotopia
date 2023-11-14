a24e1c525494b12ea90474592dfb9a027180a629
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-11-14 12:10:11 +0300

    Created a unit - part of a module

---
+++ server/src/core/models/units.py
@@ -0,0 +1,15 @@
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+
+from core.models.entity import Entity
+
+
+class Unit(Entity):
+    __tablename__ = "units"
+
+    name: Mapped[str]
+    done: Mapped[bool]
+    experience_amount: Mapped[int]
+
+    module: Mapped[int] = mapped_column("module_id", ForeignKey("modules.id"))
+    previous_unit: Mapped[int] = mapped_column("unit_id", ForeignKey("units.id"), nullable=True)
