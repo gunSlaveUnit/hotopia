9dfe4ff67931d32a79fefdff6a14ec2440394185
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-11-14 14:12:37 +0300

    Walkthroughes schemas

---
+++ server/src/api/v1/schemas/walkthroughes.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+
+from server.src.api.v1.schemas.entity import EntityDBSchema
+
+
+class WalkthroughBaseSchema(BaseModel):
+    user: int
+    unit: int
+    done: bool
+
+
+class WalkthroughCreateSchema(WalkthroughBaseSchema):
+    pass
+
+
+class WalkthroughDBSchema(WalkthroughCreateSchema, EntityDBSchema):
+    class Config:
+        from_attributes = True
