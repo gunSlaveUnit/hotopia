c8ab9660038a0ee8936dccb0cf8205a93bd16185
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-11-14 13:38:35 +0300

    Bound units router

--- server/src/api/v1/api.py
+++ server/src/api/v1/api.py
@@ -3,8 +3,10 @@ from fastapi import APIRouter
 from settings import API_VERSION_1_PREFIX
 from server.src.api.v1.endpoints.hobbies import router as hobbies_router
 from server.src.api.v1.endpoints.modules import router as modules_router
+from server.src.api.v1.endpoints.units import router as units_router
 
 router = APIRouter(prefix=API_VERSION_1_PREFIX)
 
 router.include_router(hobbies_router)
 router.include_router(modules_router)
+router.include_router(units_router)
