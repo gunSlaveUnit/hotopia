d1ffe5734221d4db659c806a1c4ed94d3ebe3a60
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-11-15 10:54:25 +0300

    Created and bound auth router

--- server/src/api/v1/api.py
+++ server/src/api/v1/api.py
@@ -1,14 +1,16 @@
 from fastapi import APIRouter
 
 from settings import API_VERSION_1_PREFIX
-from server.src.api.v1.endpoints.hobbies import router as hobbies_router
-from server.src.api.v1.endpoints.modules import router as modules_router
+from server.src.api.v1.endpoints.auth import router as auth_router
 from server.src.api.v1.endpoints.units import router as units_router
+from server.src.api.v1.endpoints.modules import router as modules_router
+from server.src.api.v1.endpoints.hobbies import router as hobbies_router
 from server.src.api.v1.endpoints.walthroughes import router as walkthroughes_router
 
 router = APIRouter(prefix=API_VERSION_1_PREFIX)
 
-router.include_router(hobbies_router)
-router.include_router(modules_router)
+router.include_router(auth_router)
 router.include_router(units_router)
+router.include_router(modules_router)
+router.include_router(hobbies_router)
 router.include_router(walkthroughes_router)
---
+++ server/src/api/v1/endpoints/auth.py
@@ -0,0 +1,5 @@
+from fastapi import APIRouter
+
+from settings import AUTH_ROUTER_PREFIX
+
+router = APIRouter(prefix=AUTH_ROUTER_PREFIX)
