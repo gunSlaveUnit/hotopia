3971f26a8d14b4b59e03fb617a6896755f242141
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-11-15 10:53:04 +0300

    User sign up schema

--- server/src/api/v1/schemas/users.py
+++ server/src/api/v1/schemas/users.py
@@ -4,3 +4,7 @@ from pydantic import BaseModel, EmailStr
 class UserSignInSchema(BaseModel):
     account_name: str
     password: bool
+
+
+class UserSignUpSchema(UserSignInSchema):
+    email: EmailStr
