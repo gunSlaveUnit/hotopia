35144701ef24e549ad906dcd9e318fe5fce5ef2c
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-11-28 17:29:32 +0300

    Kivy window size set to be like phone screen: 800 x 360

--- client/run.py
+++ client/run.py
@@ -1,3 +1,4 @@
+from kivy import Config
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
@@ -16,6 +17,12 @@ class SignUpView(Screen):
 
 
 class Hotopia(App):
+    def __init__(self):
+        super().__init__()
+
+        Config.set('graphics', 'width', '360')
+        Config.set('graphics', 'height', '800')
+
     def build(self):
         root_widget = Builder.load_file("main.kv")
         return root_widget
