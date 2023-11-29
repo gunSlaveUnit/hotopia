76352a8df8ff0712c94c1fbc58b4792d924e0fb5
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-11-29 14:35:09 +0300

    Long description field in a hobby schema

--- server/src/api/v1/schemas/hobbies.py
+++ server/src/api/v1/schemas/hobbies.py
@@ -6,6 +6,7 @@ from server.src.api.v1.schemas.entity import EntityDBSchema
 class HobbyBaseSchema(BaseModel):
     name: str
     short_description: str
+    long_description: str
 
 
 class HobbyCreateSchema(HobbyBaseSchema):
