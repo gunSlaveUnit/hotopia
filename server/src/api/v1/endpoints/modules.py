763d1b2903c929c052acda972dcf0126f74f014b
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-11-29 15:01:11 +0300

    Get all route for modules

--- server/src/api/v1/endpoints/modules.py
+++ server/src/api/v1/endpoints/modules.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -7,6 +9,11 @@ from server.src.settings import MODULES_ROUTER_PREFIX
 from api.v1.schemas.modules import ModuleDBSchema, ModuleCreateSchema
 
 router = APIRouter(prefix=MODULES_ROUTER_PREFIX)
+
+
+@router.get('/', response_model=List[ModuleDBSchema])
+async def items(db: AsyncSession = Depends(get_db)) -> List[Module]:
+    return [_ async for _ in Module.every(db)]
 
 
 @router.post('/', response_model=ModuleDBSchema)
