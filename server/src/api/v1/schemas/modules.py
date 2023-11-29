f142d38d8db7d569bba48e5e9aebdcfd4a5f0ac3
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-11-29 14:36:41 +0300

    Removed done field from module schema

--- server/src/api/v1/schemas/modules.py
+++ server/src/api/v1/schemas/modules.py
@@ -6,7 +6,6 @@ from server.src.api.v1.schemas.entity import EntityDBSchema
 class ModuleBaseSchema(BaseModel):
     name: str
     description: str
-    done: bool
     hobby: int
     previous_module: int | None
 
