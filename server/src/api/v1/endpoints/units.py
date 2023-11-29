c59c093512257b47b2c0ce451e5f51b2f893c0a9
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-11-29 15:01:05 +0300

    Get all route for units

--- server/src/api/v1/endpoints/units.py
+++ server/src/api/v1/endpoints/units.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -7,6 +9,11 @@ from server.src.settings import UNITS_ROUTER_PREFIX
 from api.v1.schemas.units import UnitDBSchema, UnitCreateSchema
 
 router = APIRouter(prefix=UNITS_ROUTER_PREFIX)
+
+
+@router.get('/', response_model=List[UnitDBSchema])
+async def items(db: AsyncSession = Depends(get_db)) -> List[Unit]:
+    return [_ async for _ in Unit.every(db)]
 
 
 @router.post('/', response_model=UnitDBSchema)
