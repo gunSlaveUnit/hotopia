763c8a61c2412d1758facb1200fddb3db44924c9
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-12-02 15:25:31 +0300

    File fixed name

--- server/src/api/v1/api.py
+++ server/src/api/v1/api.py
@@ -5,7 +5,7 @@ from server.src.api.v1.endpoints.auth import router as auth_router
 from server.src.api.v1.endpoints.units import router as units_router
 from server.src.api.v1.endpoints.modules import router as modules_router
 from server.src.api.v1.endpoints.hobbies import router as hobbies_router
-from server.src.api.v1.endpoints.walthroughes import router as walkthroughes_router
+from server.src.api.v1.endpoints.walkthroughes import router as walkthroughes_router
 
 router = APIRouter(prefix=API_VERSION_1_PREFIX)
 
