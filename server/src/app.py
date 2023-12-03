94b9cf90abfeb2e99570fe4b6fdf571ad2372b17
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-12-03 14:13:55 +0300

    Fixed: using MEDIA_PREFIX and MEDIA_PATH in "app.mount" statement

--- server/src/app.py
+++ server/src/app.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from starlette.staticfiles import StaticFiles
 
-from settings import DEBUG
 from core.utils.db import create as create_db
+from settings import DEBUG, MEDIA_PATH, MEDIA_PREFIX
 from server.src.api.v1.api import router as api_v1_router
 
 app = FastAPI(debug=DEBUG)
@@ -10,7 +10,7 @@ app = FastAPI(debug=DEBUG)
 app.include_router(api_v1_router)
 
 if DEBUG:
-    app.mount("/media", StaticFiles(directory="media"), name="media")
+    app.mount(MEDIA_PREFIX, StaticFiles(directory=MEDIA_PATH), name=MEDIA_PATH)
 
 
 @app.on_event("startup")
