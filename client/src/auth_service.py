0cb42691922a91d4d95dcd8b390a09f19c10d1a1
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-12-08 12:05:16 +0300

    AuthService in client to make sign_* operations. Also it controls authorized user session and a current user data

---
+++ client/src/auth_service.py
@@ -0,0 +1,46 @@
+from typing import Union, Optional
+
+import requests
+from requests import Session
+
+from core.models.users import User
+from api.v1.schemas.users import UserSignUpSchema, UserSignInSchema
+from settings import SIGN_UP_URL, SIGN_IN_URL, SIGN_OUT_URL, ME_URL
+
+
+class AuthService:
+    def __init__(self):
+        super().__init__()
+
+        self.current_user: Optional[User] = None
+        self.authorized_session:Optional[Session] = None
+
+    def _user_access(self, url: str, data: Union[UserSignUpSchema, UserSignInSchema]):
+        response = requests.post(url, json=data.model_dump())
+
+        if response.ok:
+            self.authorized_session = requests.session()
+            self.authorized_session.cookies.set('session', response.cookies['session'])
+
+        return response
+
+    def sign_up(self, data: UserSignUpSchema):
+        return self._user_access(SIGN_UP_URL, data)
+
+    def sign_in(self, data: UserSignInSchema):
+        return self._user_access(SIGN_IN_URL, data)
+
+    def sign_out(self):
+        response = self.authorized_session.post(SIGN_OUT_URL)
+
+        if response.ok:
+            self.authorized_session = None
+            self.current_user = None
+
+        return response
+
+    def load_current_user(self):
+        response = self.authorized_session.get(ME_URL)
+
+        if response.ok:
+            self.current_user = User(**response.json())
