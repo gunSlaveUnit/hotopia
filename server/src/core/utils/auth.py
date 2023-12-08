a2c30579d920ef5e02090d101ab83f20c8c07355
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-12-08 12:27:45 +0300

    Get current user func in server

--- server/src/core/utils/auth.py
+++ server/src/core/utils/auth.py
@@ -1,9 +1,12 @@
 from typing import Optional
 
+from fastapi import HTTPException, Depends, Cookie
 from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
 
 from core.models.users import User
 from core.utils.crypt import crypt_context
+from core.utils.db import get_db, get_session_storage
 
 
 async def verify_password(plain_password, hashed_password) -> bool:
@@ -19,3 +22,24 @@ async def authenticate_user(
     if user and await verify_password(password, user.password):
         return user
     return None
+
+
+async def get_current_user(
+        session: str = Cookie(None),
+        db: AsyncSession = Depends(get_db),
+        session_storage=Depends(get_session_storage),
+) -> User:
+    if session is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Session ID not provided"
+        )
+
+    user_id = await session_storage.get(session)
+    if user_id is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="The session has expired. Please re-login"
+        )
+
+    return await User.by_id(db, int(user_id))
