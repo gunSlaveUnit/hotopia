7db0b536ef96b6e909bfde06a92b114e12c30a65
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-12-13 13:52:00 +0300

    Related hobby in modules

--- server/src/core/models/modules.py
+++ server/src/core/models/modules.py
@@ -1,7 +1,14 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 from sqlalchemy import ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from core.models.entity import Entity
+
+if TYPE_CHECKING:
+    from core.models.hobbies import Hobby
 
 
 class Module(Entity):
@@ -12,3 +19,5 @@ class Module(Entity):
 
     hobby_id: Mapped[int] = mapped_column("hobby_id", ForeignKey("hobbies.id"))
     previous_module_id: Mapped[int] = mapped_column("module_id", ForeignKey("modules.id"), nullable=True)
+
+    hobby: Mapped[Hobby] = relationship("Hobby", back_populates="modules")
