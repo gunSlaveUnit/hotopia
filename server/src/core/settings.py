63afebed54fcb551fc15358cbee55b49336be09b
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-01-04 23:24:21 +0300

    Fixed base path in server settings

--- server/src/core/settings.py
+++ server/src/core/settings.py
@@ -6,5 +6,5 @@ RELOAD = True
 
 SESSION_TTL = 3 * 24 * 60 * 60
 
-BASE_PATH = Path(__file__).resolve().parent.parent.parent
+BASE_PATH = Path(__file__).resolve().parent.parent.parent.parent
 MEDIA_PATH = BASE_PATH / 'media'
