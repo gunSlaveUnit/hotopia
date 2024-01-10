e0d42a3a88b3c7d73490c0d784662204edfaa14b
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-01-10 14:17:26 +0300

    Use debug config values

--- server/src/app.py
+++ server/src/app.py
@@ -1,17 +1,16 @@
 from fastapi import FastAPI
 from starlette.staticfiles import StaticFiles
 
-from server.src.core.settings import MEDIA_PREFIX
-from server.src.core.settings import DEBUG, MEDIA_PATH
 from server.src.core.utils.db import create as create_db
 from server.src.api.v1.api import router as api_v1_router
+from server.src.core.settings import CONFIG, MEDIA_PREFIX, MEDIA_PATH
 
-app = FastAPI(debug=DEBUG)
+debug = CONFIG['DEBUG']
+app = FastAPI(debug=debug)
+if debug:
+    app.mount(MEDIA_PREFIX, StaticFiles(directory=MEDIA_PATH), name=MEDIA_PATH)
 
 app.include_router(api_v1_router)
-
-if DEBUG:
-    app.mount(MEDIA_PREFIX, StaticFiles(directory=MEDIA_PATH), name=MEDIA_PATH)
 
 
 @app.on_event("startup")
