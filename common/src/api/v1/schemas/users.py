de0bc134a679fe73636e8949d9a42f9dec72e7a9
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-01-11 12:07:30 +0300

    Created UserDBSchema

--- common/src/api/v1/schemas/users.py
+++ common/src/api/v1/schemas/users.py
@@ -1,10 +1,20 @@
+import datetime
+
 from pydantic import BaseModel, EmailStr
+
+from common.src.api.v1.schemas.entity import EntityDBSchema
 
 
 class UserSignInSchema(BaseModel):
-    account_name: str
     password: str
+    account_name: str
 
 
 class UserSignUpSchema(UserSignInSchema):
     email: EmailStr
+
+
+class UserDBSchema(EntityDBSchema, UserSignUpSchema):
+    is_active: bool
+    displayed_name: str
+    login_at: datetime.datetime | None
