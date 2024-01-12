8fc70e328ab798588a032318f3f3a729aa8dc125
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-01-12 23:00:15 +0300

    Added init file in models so models is a package now. That fixes alembic empty migrations

---
+++ server/src/core/models/__init__.py
@@ -0,0 +1,7 @@
+from server.src.core.models.base import Base
+from server.src.core.models.entity import Entity
+from server.src.core.models.hobbies import Hobby
+from server.src.core.models.modules import Module
+from server.src.core.models.units import Unit
+from server.src.core.models.users import User
+from server.src.core.models.walkthroughes import Walkthrough
