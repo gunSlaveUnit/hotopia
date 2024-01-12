37c0ca648ca31f5cc4a2f1d0efe8be528e3241bd
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-01-12 23:00:32 +0300

    Fixed Base model import

--- server/migrations/env.py
+++ server/migrations/env.py
@@ -6,7 +6,8 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from server.src.core.utils.db import engine, Base
+from server.src.core.models import Base
+from server.src.core.utils.db import engine
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
